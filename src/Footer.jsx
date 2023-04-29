43d77c3f2c36982648da5ea8f125a01474a52afd
Tanay Makharia <tanaymakharia17@gmail.com>
2023-04-29 13:20:37 +0530

    Updates

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = (props) => {
-  return (
-    <footer style={{ padding:"8px" }}>
-      <p style={{fontSize: "12px"}}>© 2023 Tanay Makharia. All rights reserved.</p>
-    </footer>
-  );
-};
-
-export default Footer;
